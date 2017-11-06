0542de0d15fc673941028f505c9823cb435955ca
MininMobile <miningamer@gmail.com>
2017-11-06 15:49:46 +0000

    Add files

---
+++ discord.bat.exe/CommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace discord.bat.exe
+{
+    class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            Process myProcess = new Process();
+
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "eventManager.bat";
+                myProcess.StartInfo.Arguments = "message \""+ msg.Content + "\"";
+
+                myProcess.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            await Task.Delay(100);
+            if (File.Exists("update.dat"))
+            {
+                var update = File.ReadAllLines("update.dat")[0];
+
+                if (update.StartsWith("send"))
+                {
+                    update = update.Substring(4);
+                    await msg.Channel.SendMessageAsync(update);
+                    File.Delete("update.dat");
+                }
+            }
+        }
+    }
+}
---
+++ discord.bat.exe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace discord.bat.exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new Program().StartAsync().GetAwaiter().GetResult();
+            }
+            catch
+            {
+                Console.WriteLine("Connection Error");
+                Console.WriteLine("You might have entered the wrong token!");
+
+                Console.ReadKey();
+            }
+        }
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, File.ReadAllLines("..\\TEMP\\bot.token.dat")[0]);
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+
+            Console.WriteLine("Connected!");
+            await Task.Delay(-1);
+        }
+    }
+}
